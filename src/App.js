7249da6671583120d6180ad4043cd5f59423cae4
Ismail51 <dassler-ben@hotmail.fr>
2022-05-13 20:32:12 +0200

    test

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Form from './Components/Form'
 function App() {
   return (
     <>
-      <Header></Header>
-      <Form></Form>
+      {/* <Header></Header>
+      <Form></Form> */}
       <footer>
         <p>Réalisé par Jason en Anthestérion de l'an 515 avant JC</p>
       </footer>
