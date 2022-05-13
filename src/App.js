e264e01a317aa245683429a7d9a1da2d45b62fe8
Ismail51 <dassler-ben@hotmail.fr>
2022-05-13 20:17:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header'
+import './App.css'
+import Form from './Components/Form'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header></Header>
+      <Form></Form>
+      <footer>
+        <p>Réalisé par Jason en Anthestérion de l'an 515 avant JC</p>
+      </footer>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Form.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { useState, useEffect } from 'react'
+import Squad from './Squad'
+
+function Form() {
+  const [liste, setList] = useState([])
+  const getElement = () => {
+    axios.get('http://localhost:3007/').then(data => {
+      console.log(data);
+      setList(data.data)
+    })
+  }
+
+  useEffect(() => {
+    getElement()
+  }, [])
+  const [name, setName] = useState('')
+  const [error, setError] = useState(false)
+
+  const sendData = (e) => {
+    e.preventDefault()
+    console.log('ookok');
+    axios({
+      method: 'post',
+      url: 'http://localhost:3007/',
+      data: {
+        name: name
+      }
+    }).catch(error => {
+      console.log(error.response.status);
+      setError(true)
+    }).then(data => {
+      getElement()
+    })
+  }
+
+  return (
+    <>
+      <h2>Ajouter un(e) Argonaute</h2>
+      <form onSubmit={sendData} class="new-member-form">
+        {
+          error ?
+            <div className="error">
+              <p>erreur</p>
+            </div>
+            : null
+        }
+        <label for="name">Nom de l&apos;Argonaute</label>
+        <input onChange={(e) => { setName(e.target.value) }} id="name" name="name" type="text" placeholder="Charalampos" />
+        <button type="submit">Envoyer</button>
+      </form>
+      <h2>Membres de l'équipage</h2>
+      <section class="member-list">
+        {
+          liste.map(element => {
+            return (
+              <div class="member-item">{element.name}</div>
+            )
+
+          })
+        }
+      </section>
+    </>
+
+  )
+}
+
+export default Form
